5333fd56afaf7a03fc41d081cf2f7df42e56b2e3
Alban Dusaussoy <alban.dusaussoy@lifen.fr>
2025-04-11 10:35:57 +0200

    nvim: update & config fix

--- config/nvim/lua/plugins/conform.lua
+++ config/nvim/lua/plugins/conform.lua
@@ -4,12 +4,12 @@ return {
     require("conform").setup({
       formatters_by_ft = {
         -- Use a sub-list to run only the first available formatter
-        javascript = { { "biome-check", "prettierd", "prettier" } },
-        typescript = { { "biome-check", "prettierd", "prettier" } },
-        typescriptreact = { { "biome-check", "prettierd", "prettier" } },
-        javascriptreact = { { "biome-check", "prettierd", "prettier" } },
-        css = { { "prettierd", "prettier" } },
-        json = { { "prettier" } },
+        javascript = { "biome-check", "prettierd", "prettier" },
+        typescript = { "biome-check", "prettierd", "prettier" },
+        typescriptreact = { "biome-check", "prettierd", "prettier" },
+        javascriptreact = { "biome-check", "prettierd", "prettier" },
+        css = { "prettierd", "prettier" },
+        json = { "prettier" },
       },
       -- format_on_save = {
       --   -- These options will be passed to conform.format()
